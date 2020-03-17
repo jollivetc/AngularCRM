a14941bf074cd48ea6c99ca96b50ee1f8dbfb94a
Christophe Jollivet <christophe.jollivet@gmail.com>
2020-03-17 10:43:07 +0100

    add more tests

--- src/app/login/authentication.service.spec.ts
+++ src/app/login/authentication.service.spec.ts
@@ -1,14 +1,55 @@
-import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TestBed, async } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { AuthenticationService } from './authentication.service';
+import { User } from './model/user';
 
 describe('AuthenticationService', () => {
+  let httpTestingController: HttpTestingController;
+
   beforeEach(() => TestBed.configureTestingModule({
     imports: [HttpClientTestingModule]
   }));
-
+  beforeEach(() => {
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+  afterEach(() => {
+    // After every test, assert that there are no more pending requests.
+    httpTestingController.verify();
+  });
   it('should be created', () => {
     const service: AuthenticationService = TestBed.inject(AuthenticationService);
     expect(service).toBeTruthy();
   });
+  it('should call the server and transform the response and store the user and token', async(() => {
+    //prepare the server response.
+    const user: User = {
+      id: 42,
+      login: 'login',
+      lastname: 'lastname',
+      firstname: 'firstname'
+    };
+    const response = {
+      user: user,
+      token: 'aToken'
+    };
+    const service: AuthenticationService = TestBed.inject(AuthenticationService);
+    service.authentUser('login', 'password').subscribe( data => {
+      // assert treatment of response.
+      expect(data).toBe(user);
+      expect(service.authenticated).toBeTrue();
+      expect(service.token).toBe('aToken');
+    });
+    // check the query sent.
+    const req = httpTestingController.expectOne('/api/auth/login');
+    expect(req.request.method).toEqual('POST');
+    expect(req.request.body).toEqual({email:'login', password: 'password'});
+
+    // Respond with mock data, causing Observable to resolve.
+    // Subscribe callback asserts that correct data was returned.
+    req.flush(response);
+  }));
+
+  it('should retrieve existing user and token from the sessionStorage', () => {
+    
+  })
 });
--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -6,15 +6,61 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { AppMaterialModule } from '../app-material.module';
 import { HelpComponent } from '../component/help/help.component';
 import { LoginComponent } from './login.component';
+import { AuthenticationService } from './authentication.service';
+import { Observable, Subscriber } from 'rxjs';
+import { User } from './model/user';
+import { Router, UrlTree, NavigationExtras } from '@angular/router';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let authenticationServiceStub: Partial<AuthenticationService>;
+  let routerStub: Partial<Router>;
+
+  authenticationServiceStub = {
+    authentUser: (login: string, password: string): Observable<User> => {
+      return new Observable((subscriber: Subscriber<User>) => {
+        subscriber.next({
+          id: 12,
+          login,
+          lastname: 'Doe',
+          firstname: 'John'
+        });
+      });
+    }
+  };
+
+  routerStub = {
+    navigateByUrl: (url: string | UrlTree, extras?: NavigationExtras): Promise<boolean> => {
+      return new Promise((resolve, reject) => {
+        resolve(true);
+      });
+    }
+  };
+
+  function fillForm(): void {
+    const loginElement = fixture.nativeElement.querySelector('input#login');
+    const passwordElement = fixture.nativeElement.querySelector('input#password');
+    loginElement.value = 'myLogin';
+    loginElement.dispatchEvent(new Event('input'));
+    passwordElement.value = 'password';
+    passwordElement.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+  }
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [ReactiveFormsModule, HttpClientTestingModule, RouterTestingModule, NoopAnimationsModule, AppMaterialModule],
-      declarations: [ LoginComponent, HelpComponent ]
+      imports: [
+        AppMaterialModule,
+        NoopAnimationsModule,
+        ReactiveFormsModule,
+        RouterTestingModule
+      ],
+      declarations: [ LoginComponent, HelpComponent ],
+      providers: [
+        {provide: AuthenticationService, useValue: authenticationServiceStub},
+        {provide: Router, useValue: routerStub}
+      ]
     })
     .compileComponents();
   }));
@@ -32,15 +78,31 @@ describe('LoginComponent', () => {
     const element = fixture.nativeElement;
     expect(element.querySelector('button').disabled).toBeTrue();
   });
+
   it('should activate login button when form is valid', () => {
     const buttonElement = fixture.nativeElement.querySelector('button');
-    const loginElement = fixture.nativeElement.querySelector('input#login');
-    const passwordElement = fixture.nativeElement.querySelector('input#password');
-    loginElement.value = 'myLogin';
-    loginElement.dispatchEvent(new Event('input'));
-    passwordElement.value = 'password';
-    passwordElement.dispatchEvent(new Event('input'));
-    fixture.detectChanges();
+    fillForm();
     expect(buttonElement.disabled).toBeFalse();
   });
+
+  it('should call the authenticationService with the parameters', async(() => {
+    // need to callThrough to get an observable to be subscribed to.
+    spyOn(authenticationServiceStub, 'authentUser').and.callThrough();
+    fillForm();
+    const buttonElement = fixture.nativeElement.querySelector('button');
+    buttonElement.click();
+    fixture.whenStable().then(() => {
+      expect(authenticationServiceStub.authentUser).toHaveBeenCalledWith('myLogin', 'password');
+    });
+  }));
+  it('navigate when the authentication is alright', async(()=>{
+    // no need to call through as the returned Promise<boolean> is unused.
+    spyOn(routerStub, 'navigateByUrl');
+    // work with component and not element, no UI interaction is mandatory and it will be faster
+    component.onSubmit();
+    fixture.whenStable().then(() => {
+      expect(routerStub.navigateByUrl).toHaveBeenCalledWith('/home');
+    });
+  }));
+
 });

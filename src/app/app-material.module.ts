f45a4302c9345798a62410490b0841af83fa0516
Christophe Jollivet <jollivet@apside.fr>
2023-09-08 14:58:05 +0200

    ng update ongoing

--- src/app/app-material.module.ts
+++ src/app/app-material.module.ts
@@ -1,12 +1,12 @@
 import {NgModule} from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
+import {MatLegacyButtonModule as MatButtonModule} from '@angular/material/legacy-button';
 import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatSelectModule} from '@angular/material/select';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
+import {MatLegacySelectModule as MatSelectModule} from '@angular/material/legacy-select';
+import {MatLegacyFormFieldModule as MatFormFieldModule} from '@angular/material/legacy-form-field';
+import {MatLegacyInputModule as MatInputModule} from '@angular/material/legacy-input';
 import {MatIconModule} from '@angular/material/icon';
-import {MatTooltipModule} from '@angular/material/tooltip';
-import {MatTableModule} from '@angular/material/table';
+import {MatLegacyTooltipModule as MatTooltipModule} from '@angular/material/legacy-tooltip';
+import {MatLegacyTableModule as MatTableModule} from '@angular/material/legacy-table';
 
 const importExport = [MatButtonModule, MatToolbarModule, MatSelectModule,
   MatFormFieldModule, MatInputModule, MatIconModule, MatTooltipModule, MatTableModule];
